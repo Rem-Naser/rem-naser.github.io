9c81c581334dc3c1cf6b8c4075c5492ab6fc3afa
Rem-Naser <30963193+Rem-Naser@users.noreply.github.com>
2017-08-13 00:28:25 +0300

    Update erc20dashboard.js

--- erc20dashboard.js
+++ erc20dashboard.js
@@ -1,6 +1,6 @@
 
 	if (typeof erc20contract_address == "undefined") {
-		var erc20contract_address = "0x9671Bf5124Cb4Bd30027152FAFe8409eA46677E4";
+		var erc20contract_address = "0x34f837d221c2cd647916017a6d4ab770c469dbcb";
 		var option_etherscan_api = 'https://ropsten.etherscan.io'; //change to https://api.etherscan.io for mainnet
 		var option_registration_enabled = true;
 		var option_registration_backend = '';///'subscribe.php'; //you can use remote address like https://yoursite.com/subscribe.php
